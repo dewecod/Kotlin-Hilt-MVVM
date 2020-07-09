0cdd398f2d6b79f6cde1a6dda08e546c2e301e55
dewecod <dewecod@gmail.com>
2020-07-09 18:51:23 +0500

    Add internet permission and security config

--- app/src/main/java/com/dewecod/mvvmhilt/ui/HomeFragment.kt
+++ app/src/main/java/com/dewecod/mvvmhilt/ui/HomeFragment.kt
@@ -43,7 +43,7 @@ class HomeFragment : Fragment() {
         initRecyclerView()
         observeData()
         setUpItemTouchHelper()
-        viewModel.getPokemonList()
+        viewModel.loadPokemons()
     }
 
     private fun initRecyclerView() {
