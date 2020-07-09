431e001f7820f974ee3ba4a913fad912e11ba3ef
dewecod <dewecod@gmail.com>
2020-07-09 12:55:19 +0500

    Add NetworkModule.kt (Dependency injection - Hilt)

---
+++ app/src/main/java/com/dewecod/mvvmhilt/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.dewecod.mvvmhilt.di
+
+import com.dewecod.mvvmhilt.net.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
